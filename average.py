999816dfc7259b6c7a94ff5bee530cf69a491ac3
Keshav12aug <keshavdubey12aug@gmail.com>
2025-05-23 16:50:08 +0530

    add this as an average calc

---
+++ average.py
@@ -0,0 +1,11 @@
+def average():
+    a = int(input("Enter the number 1 : "))
+    b = int(input("Enter the number 2 : "))
+    c = int(input("Enter the number 3 : "))
+
+    avg = (a + b + c)/3
+
+    print("The average of 3 numbers is : ", avg)
+    
+average()
+average()
